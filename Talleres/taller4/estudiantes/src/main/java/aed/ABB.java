d23d6b73b433275e948e78ea7e4f2934cb63bd04
rama_seltzer <ramiroseltzer@gmail.com>
2023-10-24 13:49:03 -0300

    update y push archivos

---
+++ Talleres/taller4/estudiantes/src/main/java/aed/ABB.java
@@ -0,0 +1,75 @@
+package aed;
+
+import java.util.*;
+
+// Todos los tipos de datos "Comparables" tienen el método compareTo()
+// elem1.compareTo(elem2) devuelve un entero. Si es mayor a 0, entonces elem1 > elem2
+public class ABB<T extends Comparable<T>> implements Conjunto<T> {
+    // Agregar atributos privados del Conjunto
+
+    private class Nodo {
+        T data;
+        Nodo padre;
+        Nodo hijoIzquierdo;
+        Nodo hijoDerecho;
+
+        // Constructor del objeto
+        Nodo(T d) {
+            d = data;
+            hijoDerecho = null;
+            hijoIzquierdo = null;
+            padre = null;
+        }
+    }
+
+    Nodo root;
+
+    public ABB() {
+        root = null;
+    }
+
+    public int cardinal() {
+        throw new UnsupportedOperationException("No implementada aun");
+    }
+
+    public T minimo(){
+        throw new UnsupportedOperationException("No implementada aun");
+    }
+
+    public T maximo(){
+        throw new UnsupportedOperationException("No implementada aun");
+    }
+
+    public void insertar(T elem){
+        throw new UnsupportedOperationException("No implementada aun");
+    }
+
+    public boolean pertenece(T elem){
+        throw new UnsupportedOperationException("No implementada aun");
+    }
+
+    public void eliminar(T elem){
+        throw new UnsupportedOperationException("No implementada aun");
+    }
+
+    public String toString(){
+        throw new UnsupportedOperationException("No implementada aun");
+    }
+
+    private class ABB_Iterador implements Iterador<T> {
+        private Nodo _actual;
+
+        public boolean haySiguiente() {            
+            throw new UnsupportedOperationException("No implementada aun");
+        }
+    
+        public T siguiente() {
+            throw new UnsupportedOperationException("No implementada aun");
+        }
+    }
+
+    public Iterador<T> iterador() {
+        return new ABB_Iterador();
+    }
+
+}
